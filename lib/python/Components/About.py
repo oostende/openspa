af2dcc62600f0f4cb7b7b12d3173aa3deb2dcc8e
darkmantk <darkmantk@gmail.com>
2013-12-21 19:13:10 +0100

    [About] Added getModelString

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -40,6 +40,15 @@ def getChipSetString():
 	except:
 		return "unavailable"
 
+def getModelString():	
+	try:
+		file = open("/proc/stb/info/boxtype", "r")
+		model = file.readline().strip()
+		file.close()
+		return model
+	except IOError:
+		return "unknown"
+
 def getCPUString():
 	try:
 		file = open('/proc/cpuinfo', 'r')
