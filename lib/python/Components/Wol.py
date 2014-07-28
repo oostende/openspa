78094cdc1058fa5ad258096fa878daefa5b03136
darkmantk <darkmantk@gmail.com>
2014-07-28 20:17:17 +0200

    WOL changes GigaBlue

--- lib/python/Components/Wol.py
+++ lib/python/Components/Wol.py
@@ -20,20 +20,26 @@ class WOL:
 
 def Init():
 	config.network = ConfigSubsection()
-	if SystemInfo["WakeOnLAN"] and not getBoxType() in ('gbquad', 'gbquadplus'):
+	if SystemInfo["WakeOnLAN"] and not getBoxType() in ('gbquadplus', 'gbquad', 'gb800ueplus', 'gb800seplus', 'gbipbox'):
 		def setWOLmode(value):
 			iwol.setWolState(config.network.wol.value)
-
 		iwol = WOL()
 		config.network.wol = ConfigSelection([("disable", _("No")), ("enable", _("Yes"))], default = "disable")
 		config.network.wol.addNotifier(setWOLmode, initial_call=True)
+
 	elif SystemInfo["ETWOL"]:
 		def setWOLmode(value):
 			iwol.setWolState(config.network.wol.value)
-
 		iwol = WOL()
 		config.network.wol = ConfigSelection([("off", _("No")), ("on", _("Yes"))], default = "off")
-		config.network.wol.addNotifier(setWOLmode, initial_call=True)		
+		config.network.wol.addNotifier(setWOLmode, initial_call=True)
+
+	elif SystemInfo["WakeOnLAN"] and getBoxType() in ('gbquadplus', 'gbquad', 'gb800ueplus', 'gb800seplus', 'gbipbox'):
+		def setWOLmode(value):
+			iwol.setWolState(config.network.wol.value)
+		iwol = WOL()
+		config.network.wol = ConfigSelection([("off", _("No")), ("on", _("Yes"))], default = "off")
+		config.network.wol.addNotifier(setWOLmode, initial_call=True)
 	else:
 		def doNothing():
 			pass
