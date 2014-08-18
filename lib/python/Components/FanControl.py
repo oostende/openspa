e68b2900d7f2f0d04e4cbb3e7f1736ab8fc0ba8a
darkmantk <darkmantk@gmail.com>
2014-08-18 18:47:18 +0200

    Remove redundant file.close calls and variable assignment
    Calling close() on files is redundant, files will automatically close when
    going out of scope. Something like:
    
    f = open(name,"r")
    value = int(f.readline())
    f.close()
    
    is just a waste of resources, writing:
    
    value = int(open(name,"r").readline())
    
    is more efficient because it doesn't need to store and retrieve objects from
    the namespace.
    
    A file is an iterator, so instead of file.readlines() one can usually use the
    file itself, instead of first reading it into a list. This also saves on
    resources.

--- lib/python/Components/FanControl.py
+++ lib/python/Components/FanControl.py
@@ -58,7 +58,7 @@ class FanControl:
 			fancontrol.setVoltage(fanid, configElement.value)
 		def setPWM(fancontrol, fanid, configElement):
 			fancontrol.setPWM(fanid, configElement.value)
-		
+
 		config.fans = ConfigSubList()
 		for fanid in range(self.getFanCount()):
 			fan = ConfigSubsection()
@@ -69,49 +69,36 @@ class FanControl:
 			fan.vlt.addNotifier(boundFunction(setVlt, self, fanid))
 			fan.pwm.addNotifier(boundFunction(setPWM, self, fanid))
 			config.fans.append(fan)
-	
+
 	def getConfig(self, fanid):
 		return config.fans[fanid]
-	
+
 	def getFanCount(self):
 		return self.fancount
-	
+
 	def hasRPMSensor(self, fanid):
 		return os.path.exists("/proc/stb/fp/fan_speed")
-	
+
 	def hasFanControl(self, fanid):
 		return os.path.exists("/proc/stb/fp/fan_vlt") or os.path.exists("/proc/stb/fp/fan_pwm")
-	
+
 	def getFanSpeed(self, fanid):
-		f = open("/proc/stb/fp/fan_speed", "r")
-		value = int(f.readline().strip()[:-4])
-		f.close()
-		return value
-	
+		return int(open("/proc/stb/fp/fan_speed", "r").readline().strip()[:-4])
+
 	def getVoltage(self, fanid):
-		f = open("/proc/stb/fp/fan_vlt", "r")
-		value = int(f.readline().strip(), 16)
-		f.close()
-		return value
-	
+		return int(open("/proc/stb/fp/fan_vlt", "r").readline().strip(), 16)
+
 	def setVoltage(self, fanid, value):
 		if value > 255:
 			return
-		f = open("/proc/stb/fp/fan_vlt", "w")
-		f.write("%x" % value)
-		f.close()
-		
+		open("/proc/stb/fp/fan_vlt", "w").write("%x" % value)
+
 	def getPWM(self, fanid):
-		f = open("/proc/stb/fp/fan_pwm", "r")
-		value = int(f.readline().strip(), 16)
-		f.close()
-		return value
-	
+		return int(open("/proc/stb/fp/fan_pwm", "r").readline().strip(), 16)
+
 	def setPWM(self, fanid, value):
 		if value > 255:
 			return
-		f = open("/proc/stb/fp/fan_pwm", "w")
-		f.write("%x" % value)
-		f.close()
-	
+		open("/proc/stb/fp/fan_pwm", "w").write("%x" % value)
+
 fancontrol = FanControl()
