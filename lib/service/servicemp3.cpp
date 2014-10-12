2b273a68a5ef3e89a899c1d88319b9ce61c07c89
darkmantk <darkmantk@gmail.com>
2014-10-12 21:27:20 +0200

    [servicemp3] remove an annoy eDebug

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -2351,7 +2351,7 @@ exit:
 
 	m_subtitle_sync_timer->start(next_timer, true);
 
-	eDebug("\n\n");
+	//eDebug("\n\n");
 }
 
 RESULT eServiceMP3::enableSubtitles(iSubtitleUser *user, struct SubtitleTrack &track)
