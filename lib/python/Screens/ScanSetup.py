56b123c0169557d4e95794190f20759b41e6763b
darkmantk <darkmantk@gmail.com>
2014-11-06 03:24:32 +0100

    [ScanSetup] fix typo.

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -616,7 +616,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 			if n.config_mode in ("loopthrough", "satposdepends"):
 				root_id = nimmanager.sec.getRoot(n.slot_id, int(n.config.connectedTo.value))
 				if n.type == nimmanager.nim_slots[root_id].type: # check if connected from a DVB-S to DVB-S2 Nim or viceversa
-				continue
+					continue
 			nim_list.append((str(n.slot), n.friendly_full_description))
 
 		self.scan_nims = ConfigSelection(choices = nim_list)
