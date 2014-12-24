354c31415057863ac3b0031fd75e8c021513e995
darkmantk <darkmantk@gmail.com>
2014-12-24 17:09:39 +0100

    number of rows in columns can be changed from skin as rowsincolumn parameter. Example is used there in skin_default in MemoryInfo screen.

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -11,6 +11,10 @@ from Components.Label import Label
 from Components.ProgressBar import ProgressBar
 
 from Tools.StbHardware import getFPVersion
+from enigma import eTimer, eLabel
+
+from Components.HTMLComponent import HTMLComponent
+from Components.GUIComponent import GUIComponent
 
 class About(Screen):
 	def __init__(self, session):
@@ -152,6 +156,8 @@ class MemoryInfo(Screen):
 		self["slide"] = ProgressBar()
 		self["slide"].setValue(100)
 
+		self["params"] = MemoryInfoSkinParams()
+
 		self['info'] = Label(_("This info is for developers only.\nFor a normal users it is not important.\nDon't panic, please, when here will be displayed any suspicious informations!"))
 
 		self.setTitle(_("Memory Info"))
@@ -170,7 +176,7 @@ class MemoryInfo(Screen):
 					mem = int(size)
 				if "MemFree" in name:
 					free = int(size)
-				if i < 25:
+				if i < self["params"].rows_in_column:
 					ltext += "".join((name,"\n"))
 					lvalue += "".join((size," ",units,"\n"))
 				else:
@@ -194,3 +200,19 @@ class MemoryInfo(Screen):
 		system("sync")
 		system("echo 3 > /proc/sys/vm/drop_caches")
 		self.getMemoryInfo()
+
+class MemoryInfoSkinParams(HTMLComponent, GUIComponent):
+	def __init__(self):
+		GUIComponent.__init__(self)
+		self.rows_in_column = 25
+
+	def applySkin(self, desktop, screen):
+		if self.skinAttributes is not None:
+			attribs = [ ]
+			for (attrib, value) in self.skinAttributes:
+				if attrib == "rowsincolumn":
+					self.rows_in_column = int(value)
+			self.skinAttributes = attribs
+		return GUIComponent.applySkin(self, desktop, screen)
+
+	GUI_WIDGET = eLabel
