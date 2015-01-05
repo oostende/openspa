7b156dd4810435ec90d49a55e3e9f0528765ebc2
darkmantk <darkmantk@gmail.com>
2015-01-05 14:23:30 +0100

    No longer assume DVD or CD is mounted under /autofs
    Everything mounts under /media/ and DVD or CD should not be an exception
    to that. Adapt the code to no longer assume that /autofs even exists.

--- lib/python/Plugins/Extensions/DVDPlayer/plugin.py
+++ lib/python/Plugins/Extensions/DVDPlayer/plugin.py
@@ -28,20 +28,17 @@ def filescan_open(list, session, **kwargs):
 	from Screens import DVD
 	if len(list) == 1 and list[0].mimetype == "video/x-dvd":
 		splitted = list[0].path.split('/')
-		print "splitted", splitted
 		if len(splitted) > 2:
-			if splitted[1] == 'autofs':
+			if splitted[1] == 'media' and (splitted[2].startswith('sr') or splitted[2] == 'dvd'):
 				session.open(DVD.DVDPlayer, dvd_device="/dev/%s" %(splitted[2]))
 				return
-			else:
-				print "splitted[0]", splitted[1]
 	else:
 		dvd_filelist = []
 		for x in list:
 			if x.mimetype == "video/x-dvd-iso":
 				dvd_filelist.append(x.path)
 			if x.mimetype == "video/x-dvd":
-				dvd_filelist.append(x.path.rsplit('/',1)[0])			
+				dvd_filelist.append(x.path.rsplit('/',1)[0])
 		session.open(DVD.DVDPlayer, dvd_filelist=dvd_filelist)
 
 def filescan(**kwargs):
@@ -75,7 +72,7 @@ def onPartitionChange(action, partition):
 		elif action == 'add':
 			print "[@] DVD Inserted"
 			detected_DVD = None
-		
+
 def menu(menuid, **kwargs):
 	if menuid == "mainmenu":
 		global detected_DVD
