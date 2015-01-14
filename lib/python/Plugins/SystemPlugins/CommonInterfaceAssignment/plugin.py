0af31584b24698efce0be065d7a63544b2bf5a94
darkmantk <darkmantk@gmail.com>
2015-01-14 18:13:02 +0100

    CI-Assesment: Try to solve GSOD
    As mentioned in this topic http://forums.openpli.org/topic/36486-crash-log-with-commoninterfaceassignment-27-why/page-2

--- lib/python/Plugins/SystemPlugins/CommonInterfaceAssignment/plugin.py
+++ lib/python/Plugins/SystemPlugins/CommonInterfaceAssignment/plugin.py
@@ -407,6 +407,7 @@ class myProviderSelection(ChannelSelectionBase):
 	def __init__(self, session, title):
 		ChannelSelectionBase.__init__(self, session)
 		self.onShown.append(self.__onExecCallback)
+		self.bouquet_mark_edit = EDIT_BOUQUET
 
 		self["actions"] = ActionMap(["OkCancelActions", "ChannelSelectBaseActions"],
 			{
@@ -513,6 +514,7 @@ class myChannelSelection(ChannelSelectionBase):
 	def __init__(self, session, title):
 		ChannelSelectionBase.__init__(self, session)
 		self.onShown.append(self.__onExecCallback)
+		self.bouquet_mark_edit = OFF
 
 		self["actions"] = ActionMap(["OkCancelActions", "TvRadioActions", "ChannelSelectBaseActions"],
 			{
