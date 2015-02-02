16ddb03824ca439e452c3db0a5b40a2bd7d3ef83
darkmantk <darkmantk@gmail.com>
2015-02-02 20:09:23 +0100

    Into About added date of flash receiver

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -23,6 +23,7 @@ class About(Screen):
 
 		AboutText = _("Hardware: ") + about.getHardwareTypeString() + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
+		AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
 		AboutText += _("Kernel version: ") + about.getKernelVersionString() + "\n"
 
 		EnigmaVersion = "Enigma: " + about.getEnigmaVersionString()
