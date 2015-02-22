3f83f92e448c49ff990f32d7ee919874ef34256b
darkmantk <darkmantk@gmail.com>
2015-02-22 15:50:04 +0100

    Add option to hide services that are parental protected

--- lib/python/Screens/ParentalControlSetup.py
+++ lib/python/Screens/ParentalControlSetup.py
@@ -72,6 +72,7 @@ class ParentalControlSetup(Screen, ConfigListScreen, ProtectedScreen):
 			self.list.append(self.changePin)
 			self.list.append(getConfigListEntry(_("Remember service PIN"), config.ParentalControl.storeservicepin))
 			self.list.append(getConfigListEntry(_("Protect on epg age"), config.ParentalControl.age))
+			self.list.append(getConfigListEntry(_("Hide parentel locked services"), config.ParentalControl.hideBlacklist))
 			self.reloadLists = getConfigListEntry(_("Reload blacklists"), NoSave(ConfigNothing()))
 			self.list.append(self.reloadLists)
 
