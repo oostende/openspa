0074171d9d1667b6356ad23e9fa57dfdc7da9725
darkmantk <darkmantk@gmail.com>
2015-02-26 20:23:32 +0100

    [IpkUninstall] Update more options

--- lib/python/Screens/Ipkuninstall.py
+++ lib/python/Screens/Ipkuninstall.py
@@ -145,7 +145,7 @@ class IpkuninstallList(Screen):
 			self.ipk = self.ipklist1[ires]
 			n1 = self.ipk.find("_", 0)
 			self.ipk = self.ipk[:n1]
-			self.session.openWithCallback(self.delete, ChoiceBox, title="Select method?", list=[(_("Remove"), "rem"), (_("Force Remove"), "force")])
+			self.session.openWithCallback(self.delete, ChoiceBox, title="Select method?", list=[(_("Remove"), "rem"), (_("Force Depends"), "force-depends"), (_("Force Remove"), "force-remove")])
 		else:
 			return
 
@@ -158,7 +158,10 @@ class IpkuninstallList(Screen):
 		if answer[1] == "rem":            
 			cmd = "opkg remove " + self.ipk
 			title = _("Removing ipk %s" %(self.ipk))
-		elif answer[1] == "force":            
+		elif answer[1] == "force-depens":            
+			cmd = "opkg remove --autoremove --force-depends " + self.ipk
+			title = _("Force Removing ipk %s" %(self.ipk))
+		elif answer[1] == "force-remove":            
 			cmd = "opkg remove --force-remove " + self.ipk
 			title = _("Force Removing ipk %s" %(self.ipk))
 		self.session.open(Console,_(title),[cmd])
