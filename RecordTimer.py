e5c1f0f030c1b9395ac1afceb1123649bf5575ca
darkmantk <darkmantk@gmail.com>
2015-07-31 17:50:06 +0200

    Recordings - add option config.recording.ecm_data and config.recording.include_ait

--- RecordTimer.py
+++ RecordTimer.py
@@ -166,8 +166,21 @@ class RecordTimerEntry(timer.TimerEntry, object):
 		self.autoincrease = False
 		self.autoincreasetime = 3600 * 24 # 1 day
 		self.tags = tags or []
-		self.descramble = descramble
-		self.record_ecm = record_ecm
+
+		if descramble == 'notset' and record_ecm == 'notset':
+			if config.recording.ecm_data.value == 'descrambled+ecm':
+				self.descramble = True
+				self.record_ecm = True
+			elif config.recording.ecm_data.value == 'scrambled+ecm':
+				self.descramble = False
+				self.record_ecm = True
+			elif config.recording.ecm_data.value == 'normal':
+				self.descramble = True
+				self.record_ecm = False
+		else:
+			self.descramble = descramble
+			self.record_ecm = record_ecm
+
 		self.rename_repeat = rename_repeat
 		self.needChangePriorityFrontend = config.usage.recording_frontend_priority.value != "-2" and config.usage.recording_frontend_priority.value != config.usage.frontend_priority.value
 		self.change_frontend = False
