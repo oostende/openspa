011fe1ebf2204c8c002e82f29f6ea07617894a09
darkmantk <darkmantk@gmail.com>
2015-11-29 16:49:52 +0100

    [AVSwitch] Fix typo

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -509,7 +509,7 @@ def InitAVSwitch():
 			except:
 				pass
 		config.av.hdmicolorspace = ConfigSelection(choices={
-				"Edid(Auto": _("Auto"),
+				"Edid(Auto)": _("Auto"),
 				"Hdmi_Rgb": _("RGB")},
 				default = "Edid(Auto)")
 		config.av.hdmicolorspace.addNotifier(setHDMIColorspace)
