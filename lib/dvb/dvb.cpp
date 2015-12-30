82f9b28e1b4e6d3488820e1a2eacb0c9abbcf5f6
openspa <openspateam@gmail.com>
2015-12-30 12:45:34 +0100

    Revert "CA: When descramling iterate demuxes in ascending order "

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -1093,7 +1093,7 @@ RESULT eDVBResourceManager::allocateDemux(eDVBRegisteredFrontend *fe, ePtr<eDVBA
 					}
 				}
 			}
-			if (fe || (cap & iDVBChannel::capDecode))
+			if (fe)
 			{
 				++i;
 			}
