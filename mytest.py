3dccec4f2ae90cdd623b25e6b095ce60ed0500a0
openspa <openspateam@gmail.com>
2016-01-08 13:30:46 +0100

    mytest.py: Check if self.summary has value
    After commit ae7f6ae it is possible to get the following error when osd3dsetup is not installed.
    
    File "/usr/lib/enigma2/python/mytest.py", line 315, in pushSummary
        self.summary.hide()
    AttributeError: 'list' object has no attribute 'hide'
    
    Checking if self.summary has value fixes the issue.

--- mytest.py
+++ mytest.py
@@ -288,7 +288,7 @@ class Session:
 		self.current_dialog.restoreKeyboardMode()
 		self.current_dialog.hide()
 
-		if last:
+		if last and self.summary:
 			self.current_dialog.removeSummary(self.summary)
 			self.popSummary()
 
@@ -380,7 +380,7 @@ class Session:
 		self.execEnd()
 
 	def pushSummary(self):
-		if self.summary is not None:
+		if self.summary:
 			self.summary.hide()
 		self.summary_stack.append(self.summary)
 		self.summary = None
