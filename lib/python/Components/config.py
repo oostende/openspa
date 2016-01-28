04d28c9825252014f9ff0803c2dd0473d6db540c
openspa <openspateam@gmail.com>
2016-01-28 18:26:14 +0100

    config/ConfigNumber: add use addNotifier if change value

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -1128,6 +1128,7 @@ class ConfigNumber(ConfigText):
 				self.allmarked = False
 			self.insertChar(newChar, self.marked_pos, False)
 			self.marked_pos += 1
+			self.changed()
 		else:
 			ConfigText.handleKey(self, key)
 		self.conform()
