b3428f4bb182c161f2c579569a1e6a01022b60b3
openspa <openspateam@gmail.com>
2016-02-08 18:23:33 +0100

    ChannelSelection: togglePipzap if the cursor is not equal to the curr…
    …ent service, try to search for bouquets

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -2092,6 +2092,8 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
 			lastservice = eServiceReference(self.lastservice.value)
 			if lastservice.valid() and self.getCurrentSelection() != lastservice:
 				self.setCurrentSelection(lastservice)
+				if self.getCurrentSelection() != lastservice:
+					self.servicelist.setCurrent(lastservice)
 
 			title += _(" (TV)")
 		else:
