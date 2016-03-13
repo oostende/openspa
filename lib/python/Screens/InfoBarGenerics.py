75905e3e9b562e49acada07f08673029c0d98d37
openspa <openspateam@gmail.com>
2016-03-13 18:35:20 +0100

    Revert "showPluginBrowser"

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2016,7 +2016,6 @@ class InfoBarExtensions:
 		self["InstantExtensionsActions"] = HelpableActionMap(self, "InfobarExtensions",
 			{
 				"extensions": (self.showExtensionSelection, _("Show extensions...")),
-				"showPluginBrowser": (self.showPluginBrowser, _("Show the plugin browser..")),
 			}, 1) # lower priority
 
 		self.addExtension(extension = self.getOsd3DSetup, type = InfoBarExtensions.EXTENSION_LIST)
@@ -2119,10 +2118,6 @@ class InfoBarExtensions:
 	def extensionCallback(self, answer):
 		if answer is not None:
 			answer[1][1]()
-
-	def showPluginBrowser(self):
-		from Screens.PluginBrowser import PluginBrowser
-		self.session.open(PluginBrowser)
 
 	def open3DSetup(self):
 		from Screens.UserInterfacePositioner import OSD3DSetupScreen
