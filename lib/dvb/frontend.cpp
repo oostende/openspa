20f44d3ae2a407e5af7e155813cabeaa60e962ff
openspa <openspateam@gmail.com>
2016-03-15 16:00:49 +0100

    fix build issue dvb api < 5 multistream

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -2597,7 +2597,11 @@ bool eDVBFrontend::setSlotInfo(int id, const char *descr, bool enabled, bool isD
 
 bool eDVBFrontend::is_multistream()
 {
+#if DVB_API_VERSION >= 5
 	return fe_info.caps & FE_CAN_MULTISTREAM;
+#else //if DVB_API_VERSION < 5
+	return fe_info.caps & false;
+#endif
 }
 
 std::string eDVBFrontend::getCapabilities()
