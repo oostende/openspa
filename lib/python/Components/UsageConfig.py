c27d67c9c5618f7bc7cebc2fb47b49f1187e6ebc
openspa <openspateam@gmail.com>
2016-05-16 19:41:09 +0200

    [Harddisk] Added Use Harddisk Hardware Standby Timer

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -85,6 +85,7 @@ def InitUsageConfig():
 		choicelist.append((str(i), ngettext("%d hour", "%d hours", h) % h))
 	config.usage.hdd_standby = ConfigSelection(default = "300", choices = [("0", _("No standby"))] + choicelist)
 	config.usage.hdd_standby_in_standby = ConfigSelection(default = "-1", choices = [("-1", _("Same as in active")), ("0", _("No standby"))] + choicelist)
+	config.usage.hdd_timer = ConfigYesNo(default = False)
 	config.usage.output_12V = ConfigSelection(default = "do not change", choices = [
 		("do not change", _("Do not change")), ("off", _("Off")), ("on", _("On")) ])
 
