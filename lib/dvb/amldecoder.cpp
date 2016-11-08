5476333097233220260964f4dab32e6d0f827b39
openspa <openspateam@gmail.com>
2016-11-08 21:30:37 +0100

    [wetek] amldecoder: add H265 (HEVC)

--- lib/dvb/amldecoder.cpp
+++ lib/dvb/amldecoder.cpp
@@ -222,6 +222,12 @@ RESULT eAMLTSMPEGDecoder::setVideoPID(int vpid, int type)
 				m_codec.video_type = VFORMAT_MPEG4; //maybe?
 				t = "MPEG4 Part2";
 				break;
+#if defined(__aarch64__)
+			case H265_HEVC:
+				m_codec.video_type = VFORMAT_HEVC; //maybe?
+				eDebug("%s() video type: HEVC",__PRETTY_FUNCTION__);
+				break;
+#endif
 		}
 		eDebug("[eAMLTSMPEGDecoder::setVideoPID] vpid=%d, type=%d %s", vpid, type, t.c_str());
 	}
