5646fbbe53e93628682c8b4d85a2cfe2baec92c1
openspa <openspateam@gmail.com>
2016-11-24 20:06:13 +0100

    Move InitSetupDevices to the beginning of mytest
    InitSetupDevices activate the translation and therefore it is important to enable it as soon as possible.
    This fix translation for example in NimManager where for the configs are used constructed values, but due to the fact that translation is not yet enabled the translation of these configs not working.
    Also set the time zone is better before the other component start.

--- mytest.py
+++ mytest.py
@@ -18,6 +18,11 @@ enigma.eConsoleAppContainer = eConsoleImpl.eConsoleAppContainer
 boxtype = getBoxType()
 
 from traceback import print_exc
+
+profile("SetupDevices")
+import Components.SetupDevices
+Components.SetupDevices.InitSetupDevices()
+
 profile("SimpleSummary")
 from Screens import InfoBar
 from Screens.SimpleSummary import SimpleSummary
@@ -624,10 +629,6 @@ profile("InputDevice")
 import Components.InputDevice
 Components.InputDevice.InitInputDevices()
 import Components.InputHotplug
-
-profile("SetupDevices")
-import Components.SetupDevices
-Components.SetupDevices.InitSetupDevices()
 
 profile("AVSwitch")
 import Components.AVSwitch
