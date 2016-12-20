1a4f4d312c978b07eb888aaaf1781afdfcd7b615
openspa <openspateam@gmail.com>
2016-12-20 10:35:15 +0100

    [AudioSelection] Update file

--- lib/python/Screens/AudioSelection.py
+++ lib/python/Screens/AudioSelection.py
@@ -670,8 +670,8 @@ class QuickSubtitlesConfigMenu(ConfigListScreen, Screen):
 
 		self["actions"] = NumberActionMap(["SetupActions"],
 		{
-			"cancel": self.finish,
-			"ok": self.finish,
+			"cancel": self.cancel,
+			"ok": self.ok,
 			"1": self.keyNumber,
 			"3": self.keyNumber,
 			"4": self.keyNumber,
@@ -710,7 +710,7 @@ class QuickSubtitlesConfigMenu(ConfigListScreen, Screen):
 			delay += 90000 * 30 # +30sec
 		elif number == 0:
 			delay = 0 # reset to "No delay"
-			
+
 		delay = min(max(delay, minDelay), maxDelay)
 
 		configItem.setValue(str(delay))
@@ -739,5 +739,9 @@ class QuickSubtitlesConfigMenu(ConfigListScreen, Screen):
 			return "%6.3f" % (fps/1000.)
 		return ""
 
-	def finish(self):
+	def cancel(self):
+		self.close()
+
+	def ok(self):
+		config.subtitles.save()
 		self.close()
