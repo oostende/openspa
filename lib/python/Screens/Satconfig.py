3f5fb81ba2db899d6bfc8f0af8630c6a56bbe25b
openspa <openspateam@gmail.com>
2017-01-04 16:11:02 +0100

    Only stop service in satconfig when this is really required

--- lib/python/Screens/Satconfig.py
+++ lib/python/Screens/Satconfig.py
@@ -560,6 +560,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 		return checkRecursiveConnect(self.slotid)
 
 	def keyOk(self):
+		self.stopService()
 		if self["config"].getCurrent() == self.advancedSelectSatsEntry:
 			conf = self.nimConfig.advanced.sat[int(self.nimConfig.advanced.sats.value)].userSatellitesList
 			self.session.openWithCallback(boundFunction(self.updateConfUserSatellitesList, conf), SelectSatsEntryScreen, userSatlist=conf.value)
@@ -621,13 +622,12 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 				self.deleteConfirmed(confirmed)
 			break
 		else:
-			self.restoreService(_("Zap back to service before tuner setup?"))
+			self.restartPrevService()
 
 	def __init__(self, session, slotid):
 		Screen.__init__(self, session)
 		self.list = [ ]
 		ServiceStopScreen.__init__(self)
-		self.stopService()
 		ConfigListScreen.__init__(self, self.list)
 
 		self["key_red"] = Label(_("Cancel"))
@@ -690,7 +690,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 		if self["config"].isChanged():
 			self.session.openWithCallback(self.cancelConfirm, MessageBox, _("Really close without saving settings?"))
 		else:
-			self.restoreService(_("Zap back to service before tuner setup?"))
+			self.restartPrevService()
 
 	def saveAll(self):
 		if self.nim.isCompatible("DVB-S"):
@@ -716,7 +716,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 			x[1].cancel()
 		# we need to call saveAll to reset the connectedTo choices
 		self.saveAll()
-		self.restoreService(_("Zap back to service before tuner setup?"))
+		self.restartPrevService()
 
 	def changeConfigurationMode(self):
 		if self.configMode:
