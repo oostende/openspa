2c05464a0d6020e2941e079198f96fc1dd490278
openspa <openspateam@gmail.com>
2017-02-02 17:35:37 +0100

    Big changes into servicemp3.cpp.
     Major changes into the main e2 gstreamer player.
     This change requires the latest version of dvbmediasink.
     The sinks will now always be made to use dvbmediasink.
     This avoids the use of autosink which could sometimes use the alsasink.
     The mem leak by each reinitiation of sinks is gone.
     The sinks will be made up on the first media who plays.
     Later on that sink will remain in use as long e2 is running.
     When playing audio only media the audio sink will be synchronized with gstreamer,
     This gives a better and correct start stop and next media for all media inclusif very short.
     Nice for audio playlists.
     The buffering event has been removed and is not sent to the py plugins anymore.
     This event was far to cpu intensif and sometimes the cause of non start from live media.
     However the buffering is well still used normally for the rest. But the flashy message
     Buffering will not be showed anymore.
     The http for libsouphttp has been set to 10 seconds. anyway it does 3 try outs
     This means that You wait 30 seconds. But if the problem was from dns server origin
     At least You retry a dns search. And the typical hours on which dns server timeouts do
     occur are between +- 3 pm till 11 pm when the internet is used a lot. This improves
     continuity at internet peak moments especially for hls streams.
     It could be that for rtmp or rtsp this must be changed (but not for http).
     TO DO :
     At this time I set the sink factory make into a static var and static void function to make them.
     Eventually make a clobal class cpp class for it so that external players can use the same sinks.
     and way of work. Example the gstplayer used with the ipkplayer plugin. and ...
    
    	modified:   lib/service/servicemp3.cpp
    	modified:   lib/service/servicemp3.h

--- lib/service/servicemp3.h
+++ lib/service/servicemp3.h
@@ -26,6 +26,7 @@ public:
 	RESULT list(const eServiceReference &, ePtr<iListableService> &ptr);
 	RESULT info(const eServiceReference &, ePtr<iStaticServiceInformation> &ptr);
 	RESULT offlineOperations(const eServiceReference &, ePtr<iServiceOfflineOperations> &ptr);
+	gint m_eServicemp3_counter;
 private:
 	ePtr<eStaticServiceMP3Info> m_service_info;
 };
@@ -237,10 +238,11 @@ public:
 	{
 		audiotype_t audiotype;
 		containertype_t containertype;
+		bool is_audio;
 		bool is_video;
 		bool is_streaming;
 		sourceStream()
-			:audiotype(atUnknown), containertype(ctNone), is_video(FALSE), is_streaming(FALSE)
+			:audiotype(atUnknown), containertype(ctNone), is_audio(FALSE), is_video(FALSE), is_streaming(FALSE)
 		{
 		}
 	};
@@ -324,7 +326,7 @@ private:
 		stIdle, stRunning, stStopped,
 	};
 	int m_state;
-	GstElement *m_gst_playbin, *audioSink, *videoSink;
+	GstElement *m_gst_playbin;
 	GstTagList *m_stream_tags;
 
 	eFixedMessagePump<ePtr<GstMessageContainer> > m_pump;
@@ -372,7 +374,7 @@ private:
 	void pullSubtitle(GstBuffer *buffer);
 	void sourceTimeout();
 	sourceStream m_sourceinfo;
-	gulong m_subs_to_pull_handler_id;
+	gulong m_subs_to_pull_handler_id, m_notify_source_handler_id, m_notify_element_added_handler_id;
 
 	RESULT seekToImpl(pts_t to);
 
